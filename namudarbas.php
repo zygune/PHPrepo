1a882c334cf30a6306dc9e6a07ba19f6294cb91f
Zygune <zzzygius@gmail.com>
2017-12-18 12:28:17 +0200

    Masyvai

---
+++ namudarbas.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Masyvai</title>
+</head>
+<body>
+
+<?php
+/*
+	// Duotas masyvas a. Suskaičiuokite vidutinę masyvo
+	// elementų reikšmę
+
+	$a = [3, 2, 7, 4];
+	$result = $a[0];
+	for ($i=1; $i < count($a) ; $i++) {
+		$result += $a[$i];
+	}
+	echo $result/count($a);
+*/
+
+/*  Duotas masyvas a. Suskaičiuokite lyginių indeksų masyvo elementų sumą
+
+	$sum = 0;
+	$a = [3, 2, 7, 4];
+	for ($i=0; $i < count($a); $i++){
+		if ($a[$i] % 2 == 0) {
+			$sum += $a[$i];
+		}
+	}echo $sum;
+*/
+
+
+/*//Didejimo tvarka
+$a = [3, 2, 7, 4];
+
+sort($a);
+for ($i = 0; $i < count($a); $i++) {
+    echo "$a[$i]<br>";
+}*/
+
+//Masyvo stulpeliu suma
+
+/*$array = [
+    [3, 2, 7, 4],
+    [10, 7, 9, 5],
+    [9, 11, 8, 14]
+];
+$x = 0;
+for ($i = 0; $i < count($array[0]); $i++) {
+    for ($j = 0; $j < count($array); $j++) {
+        $x += $array[$j][$i];
+    }
+    echo "$x ";
+    $x = 0;
+}*/
+
+//Istrizainiu suma nesigauna padaryti.
+
+
+?>
+
+</body>
+</html>
